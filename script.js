5871e7ab69856436e68f8e0c0acce288aeaac2bd
Red6eard <vadim.71rus@gmail.com>
2023-09-19 15:18:22 +0300

    fix dublestart bug

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let timerId; // переменная, которая будет хранить ID таймера
-
+let isRunning = false;
 
 const startButton = document.getElementById('start');
 startButton.addEventListener('click', function () {
+    if(!isRunning){
     // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
     timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+    isRunning = true;
+    }
 });
 
 
 const stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', function () {
+    if(isRunning){
     // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
     clearInterval(timerId); // останавливаем таймер
+    isRunning = false;
+    }
 });
 
 
